6a48115d50b543603546eee3714dc8a7defc5f2b
fernandocarvalhomovin <fernando@movindigital.com.br>
2015-08-12 19:02:57 -0300

    Método run

--- src/bgtask.php
+++ src/bgtask.php
@@ -10,11 +10,42 @@ namespace movin\movin_bg_task_php;
  */
 class BgTask
 {
-	
-	public function sayHello(){
-		echo "Hello";
-		die();
-	}
-	
+    /** @var string */
+    private $pathToScript;
+
+    /**
+     * Constructor.
+     *
+     * @param string Path to php script to execute
+     */
+    public function __construct($pathToScript)
+    {
+        $this->pathToScript = $pathToScript;
+    }
+
+    /**
+     * Executes the php script in a background process.
+     *
+     * @param string $outputFile File to write the output of the process to; defaults to /dev/null
+     *
+     * @return void
+     */
+    public function run($outputFile = '/dev/null')
+    {
+		$so = strtolower(php_uname('s'));
+		
+		if(strpos($so, 'windows') === FALSE){
+			$this->pid = shell_exec(sprintf(
+	            '%s > %s 2>&1 & echo $!',
+	            'php ' . $this->pathToScript,
+	            $outputFile
+	        ));
+		}
+		else{
+			$WshShell = new \COM("WScript.Shell");
+			$oExec = $WshShell->Run("php -f \"" . $this->pathToScript . "\"", 0, false);
+		}
+		
+    }
 }
 ?>
