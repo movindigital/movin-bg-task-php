2946b3e8d2c2d281ac7b8285ee117c29da85605f
fernandocarvalhomovin <fernando@movindigital.com.br>
2015-08-12 16:23:39 -0300

    Setup

---
+++ src/bgtask.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace movin\movin_bg_task_php;
+
+/**
+ *
+ * movin-bg-task-php runs an background php script under Linux/Windows enviroment
+ *
+ * @author Fernando
+ */
+class BgTask
+{
+	
+	public function sayHello(){
+		echo "Hello";
+		die();
+	}
+	
+}
+?>
